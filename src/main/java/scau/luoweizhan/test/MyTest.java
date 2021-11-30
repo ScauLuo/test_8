65bd07805becc9ec4ac416365809141879247b15
weizhan <weizhan@qq.com>
2021-11-30 17:00:22 +0800

    hot-fix commit1

--- src/main/java/scau/luoweizhan/test/MyTest.java
+++ src/main/java/scau/luoweizhan/test/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("2222");
         System.out.println("3333");
+        System.out.println("4444");
     }
 }
