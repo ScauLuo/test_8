ff725469cf82b9fb87abda311108a26f83e8ee90
ScauLuo <94658896+ScauLuo@users.noreply.github.com>
2021-12-01 21:43:11 +0800

    pull test

--- src/main/java/scau/luoweizhan/test/MyTest.java
+++ src/main/java/scau/luoweizhan/test/MyTest.java
@@ -8,5 +8,6 @@ public class MyTest {
         System.out.println("master test");
         System.out.println("hot-test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
